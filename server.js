78369c26e35f5ff1ecbbe0ba24aa99899f0e93c5
Ilham Fathy Saputra <ilhamfathys@gmail.com>
2016-08-21 15:06:52 +0000

    Add functional server.js

--- server.js
+++ server.js
@@ -10,11 +10,10 @@ bot.onText(/\/echo (.+)/, function (msg, match) {
   var resp = match[1];
   bot.sendMessage(fromId, resp);
 });
-
 // Any kind of message
 bot.on('message', function (msg) {
   var chatId = msg.chat.id;
   // photo can be: a file path, a stream or a Telegram file_id
-  var photo = 'cats.png';
-  bot.sendPhoto(chatId, photo, {caption: 'Lovely kittens'});
+  var text = 'Hello Fathy, i Promise that i will be smart one day';
+  bot.sendMessage(chatId, text);
 });
