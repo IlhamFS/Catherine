efbb55098b91c22f2071d94c796e3f140af21485
Ilham Fathy Saputra <ilhamfathys@gmail.com>
2016-08-23 15:58:30 +0700

    Scraping Using PhantomJS and Cheerio

--- scraping.js
+++ scraping.js
@@ -1,32 +1,68 @@
-var request = require('request');
+var phantom = require('phantom');
 var cheerio = require('cheerio');
-var request = request.defaults({jar:true})
-
+var sitepage = null;
+var sitepage2 = null;
+var phInstance = null;
+var phInstance2 = null;
 var theater_name = 'ARION XXI';
-request('http://m.21cineplex.com/gui.list_theater?sid=&p=th', function (error, response, html){
-  if(! error && response.statusCode == 200){
-    var $ = cheerio.load(html);
-    $('li').each(function(i, element){
-      if($(this).parent().prev().text() != 'PLAYING AT Jakarta'){
-        var a = $(this);
-        if(a.text() == theater_name ){
-          console.log(a.text());
-          show_movies(a.children('a').attr('href'));
-        }
-    }
-    });
-  }
-})
-
-function show_movies(theater_link){
-  request('http://m.21cineplex.com/' + theater_link, function(error, response, html){
-    var $ = cheerio.load(html);
-    console.log('http://m.21cineplex.com/' + theater_link);
-    //console.log(html);
-    $('#menu_ol_schedule').each(function(i, element){
-      console.log('hooho');
-      var movie_name = $(this).children('li').children('a').text();
-      console.log(movie_name);
+phantom.create()
+    .then(instance => {
+        phInstance = instance;
+        return instance.createPage();
     })
-  })
+    .then(page => {
+        sitepage = page;
+        return page.open('http://m.21cineplex.com/gui.list_theater?sid=&p=th');
+    })
+    .then(status => {
+        console.log(status);
+        return sitepage.property('content');
+    })
+    .then(content => {
+        //console.log(content);
+        var $ = cheerio.load(content);
+        $('li').each(function(i, element){
+          if($(this).parent().prev().text() != 'PLAYING AT Jakarta'){
+            var li = $(this);
+            if(theater_name == li.text()){
+              console.log(li.children('a').attr('href'));
+              show_movies(phInstance, 'http://m.21cineplex.com/' + li.children('a').attr('href'));
+            }
+              //show_movies(a.children('a').attr('href'));
+          }
+        });
+
+    })
+    .catch(error => {
+        console.log(error);
+        phInstance.exit();
+    });
+
+function show_movies(ph, url){
+      phInstance2 = ph;
+      ph.createPage()
+      .then(page => {
+        sitepage2 = page;
+        return page.open(url);
+      })
+      .then(status => {
+        console.log(status);
+        return sitepage2.property('content');
+      })
+      .then(content => {
+          var $ = cheerio.load(content);
+          $('ol').each(function(i, element){
+            var li = $(this);
+            console.log(li.text());
+
+          });
+          sitepage2.close();
+          sitepage.close();
+          phInstance.exit();
+          phInstance2.exit();
+      })
+      .catch(error => {
+          console.log(error);
+          phInstance2.exit();
+      });
 }
